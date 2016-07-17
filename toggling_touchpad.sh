631e6120a3c2ad70a87230ac654f21c8dd0e846c
Charles Lien <lienching31240@gmail.com>
2016-07-17 14:38:04 +0800

    Changed something

--- toggling_touchpad.sh
+++ toggling_touchpad.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-device=14  # your touchpad id in xinput ( use `xinput list` to check id ) 
+device="ELAN1010:00 04F3:3012 Touchpad" # your touchpad id in xinput ( use `xinput list` to check id ) 
 state=`xinput list-props "$device" | grep "Device Enabled" | grep -o "[01]$"`
 
 if [ "$state" -eq '1' ];then
