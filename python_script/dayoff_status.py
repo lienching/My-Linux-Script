530307087c444ddfa6bf12da23c798bb020f1864
Charles Lien <lienching31240@gmail.com>
2016-09-26 22:10:03 +0800

    add 停班停課查詢

---
+++ python_script/dayoff_status.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+#coding=utf8
+import sys
+if not sys.version_info[0]==3:
+    print( "Error, You need python3" )
+    exit(1)
+
+from urllib.request import urlopen
+import lxml.html
+
+BASE_URL="http://www.dgpa.gov.tw"
+PAGE_URL="/nds.html"
+
+def main():
+    html_string=''
+    try:
+        response=urlopen("http://www.dgpa.gov.tw/nds.html")
+        if 'text/html' in response.getheader('Content-Type'):
+            html_bytes = response.read()
+            html_string = html_bytes.decode("utf-8")
+        html=lxml.html.fromstring(html_string)
+        tb1=[]
+        rows = html.cssselect("tr")
+        for row in rows:
+            tb1.append([])
+            for td in row.cssselect("td"):
+                tb1[-1].append(td.text_content())
+        del tb1[0:5];
+        tb1.pop()
+        for n in tb1:
+            for j in n:
+                if u"地區" in j:
+                    n.remove(j)
+        while True:
+            try:
+                print("輸入縣市(輸入exit 結束程式):")
+                user=input()
+                finded=False
+                if user is "exit" or "exit" in user:
+                    break
+                for n in tb1:
+                    if user in n[0]:
+                        print(n[1])
+                        finded=True
+                        break
+                if not finded:
+                    print(user + " no find!\n")
+                print("\n")
+            except KeyboardInterrupt:
+                break
+        print("Program Ended!")
+    except Exception as e:
+        print(str(e))
+
+main()
