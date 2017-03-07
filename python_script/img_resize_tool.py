15b9b5e2c5995f16c84589c20aff67dc57a34eb3
Charles Lien <lienching31240@gmail.com>
2017-03-07 23:30:06 +0800

    add read value from command-line

--- python_script/img_resize_tool.py
+++ python_script/img_resize_tool.py
@@ -1,17 +1,24 @@
+#!/usr/bin/env python3
 from PIL import Image
 from os.path import splitext
+import sys
 import glob
 def resizeFactory(x,y,name):
 	im = Image.open(name)
 	nim = im.resize((x,y), Image.BILINEAR)
-	new = splitext(name)[0]+str(x)+"x"+str(y)
+	new = splitext(name)[0]+"_"+str(x)+"x"+str(y)
 	nim.save(new+".png","PNG")
 def main():
-	name = raw_input("Enter File name:")
-	x = raw_input("weight:")
-	y = raw_input("height:")
-	print "Start Conversion"
-	resizeFactory(int(x),int(y),name)
+    if ( len(sys.argv) > 1 ):
+        name = sys.argv[1]
+        x = sys.argv[2]
+        y = sys.argv[3]
+    else:
+	    name = input("Enter File name:")
+	    x = input("weight:")
+	    y = input("height:")
+    print("Start Conversion")
+    resizeFactory(int(x),int(y),name)
 	#print "ldpi"
 	#resizeFactory(36,36,name)
 	#print "mdpi"
